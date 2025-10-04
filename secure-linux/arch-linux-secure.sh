bfa19154a3ee7e3f063528e45a1a3e2e28182846
shadow-absorber <sam.tunder@gmail.com>
2025-10-04 17:08:19 +0000

    make script a lot more readable and include comments in written files

--- secure-linux/arch-linux-secure.sh
+++ secure-linux/arch-linux-secure.sh
@@ -7,13 +7,12 @@ then
 fi
 
 echo "####### START NETWORKMANAGER SECTION #####"
+# sets networkmanager conf dir
+NetworkManagerConf="/etc/NetworkManager/conf.d/"    
 
 read -r -p "apply networkmanager config? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then  
-    # sets networkmanager conf dir
-    NetworkManagerConf="/etc/NetworkManager/conf.d/"
-    
     echo "enable privacy extensions for ipv6 in networkmanager"
     cat >> "$NetworkManagerConf/10-ip6-privacy.conf" << 'EOF'
     # enable privacy extension for ipv6
@@ -43,9 +42,11 @@ then
     # yes is already the default for scanning
     [device-mac-randomization]
     wifi.scan-rand-mac-address=yes
+
     [connection-mac-randomization]
     # Randomize MAC for every ethernet connection
     ethernet.cloned-mac-address=random
+
     # Generate a random MAC for each Wi-Fi and associate the two permanently.
     wifi.cloned-mac-address=stable
     EOF
@@ -56,32 +57,47 @@ fi
 echo "##### END NETWORKMANAGER SECTION #####"
 
 echo "##### START SYSCTL SECTION #####"
+# set sysctl conf dir
+SysctlConf="/etc/sysctl.d"
 
 read -r -p "apply kernel hardening? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     echo "kernel hardening!"
-    echo "# mitigate kernel pointer leaks"
-    echo "kernel.kptr_restrict=2" > /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# restrict dmesg to CAP_SYSLOG"
-    echo "kernel.dmesg_restrict=1" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# restrict eBPF to CAP_BPF and enable JIT hardening"
-    echo "kernel.unprivileged_bpf_disabled=1" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "net.core.bpf_jit_harden=2" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# restrict TTY line disciplines to CAP_SYS_MODULE"
-    echo "dev.tty.ldisc_autoload=0" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# restrict userfaultfd to CAP_SYS_PTRACE as it can be used for use_after_free exploits"
-    echo "vm.unprivileged_userfaultfd=0" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# disable loading a new kernel while running"
-    echo "kernel.kexec_load_disabled=1" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# disable sysrq as it can be used remotely"
-    echo "kernel.sysrq=0" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# disable namespaces for none CAP_SYS_ADMIN"
-    echo "kernel.unprivileged_userns_clone=0" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# restricts perf events to CAP_PERFMON"
-    echo "kernel.perf_event_paranoid=3" >> /etc/sysctl.d/10-kernel-hardening.conf
-    echo "# only use swap if needed as it can leak sensitive data"
-    echo "vm.swappiness=1" >> /etc/sysctl.d/10-kernel-hardening.conf
+    echo "for details of what options look like check $SysctlConf/10-kernel-hardening.conf"
+    cat >> "$SysctlConf/10-kernel-hardening.conf"  << 'EOF'
+    # mitigate kernel pointer leaks
+    kernel.kptr_restrict=2
+
+    # restrict dmesg to CAP_SYSLOG
+    kernel.dmesg_restrict=1
+
+    # restrict eBPF to CAP_BPF and enable JIT hardening
+    kernel.unprivileged_bpf_disabled=1
+    net.core.bpf_jit_harden=2
+
+    # restrict TTY line disciplines to CAP_SYS_MODULE
+    dev.tty.ldisc_autoload=0
+
+    # restrict userfaultfd to CAP_SYS_PTRACE as it can be used for use_after_free exploits
+    vm.unprivileged_userfaultfd=0
+
+    # disable loading a new kernel while running
+    kernel.kexec_load_disabled=1
+    
+    # disable sysrq as it can be used remotely
+    kernel.sysrq=0
+
+    # disable namespaces for none CAP_SYS_ADMIN
+    # this is disabled as it breaks mpd
+    #kernel.unprivileged_userns_clone=0
+
+    # restricts perf events to CAP_PERFMON
+    kernel.perf_event_paranoid=3
+
+    # only use swap if needed as it can leak sensitive data
+    vm.swappiness=1
+    EOF
 else
     echo "skipping kernel hardening"
 fi
@@ -90,41 +106,54 @@ read -r -p "apply network kernel hardening? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     echo "networking hardening!"
-    echo "# protect against syn flood attacks"
-    echo "net.ipv4.tcp_syncookies=1" > /etc/sysctl.d/20-network-hardening.conf
-    echo "# drop RST packets for sockets in time-wait state"
-    echo "net.ipv4.tcp_rfc1337=1" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# protect against IP spoofing"
-    echo "net.ipv4.conf.all.rp_filter=1" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.default.rp_filter=1" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# disable icmp redirects to prevent man-in-the-middle"
-    echo "net.ipv4.conf.all.accept_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.default.accept_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.all.secure_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.default.secure_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv6.conf.all.accept_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv6.conf.default.accept_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.all.send_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.default.send_redirects=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# protect against smurf attacks"
-    echo "net.ipv4.icmp_echo_ignore_all=1" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# disable source routing to prevent man-in-the-middle"
-    echo "net.ipv4.conf.all.accept_source_route=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.conf.default.accept_source_route=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv6.conf.all.accept_source_route=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv6.conf.default.accept_source_route=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# disable ipv6 router advertisements"
-    echo "net.ipv6.conf.all.accept_ra=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv6.conf.default.accept_ra=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# disable tcp sack as it has been used commonly for exploits"
-    echo "net.ipv4.tcp_sack=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.tcp_dsack=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv4.tcp_fack=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# disable tcp timestamps"
-    echo "net.ipv4.tcp_timestamps=0" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "# enable ipv6 privacy extensions on kernel level"
-    echo "net.ipv6.conf.all.use_tempaddr=2" >> /etc/sysctl.d/20-network-hardening.conf
-    echo "net.ipv6.conf.default.use_tempaddr=2" >> /etc/sysctl.d/20-network-hardening.conf
+
+    echo "for details of what options look like check $SysctlConf/20-network-hardening.conf"
+    cat >> "$SysctlConf/20-network-hardening.conf" << 'EOF'
+    # protect against syn flood attacks
+    net.ipv4.tcp_syncookies=1
+
+    # drop RST packets for sockets in time-wait state
+    net.ipv4.tcp_rfc1337=1
+
+    # protect against IP spoofing
+    net.ipv4.conf.all.rp_filter=1
+    net.ipv4.conf.default.rp_filter=1
+
+    # disable icmp redirects to prevent man-in-the-middle
+    net.ipv4.conf.all.accept_redirects=0
+    net.ipv4.conf.default.accept_redirects=0
+    net.ipv4.conf.all.secure_redirects=0
+    net.ipv4.conf.default.secure_redirects=0
+    net.ipv6.conf.all.accept_redirects=0
+    net.ipv6.conf.default.accept_redirects=0
+    net.ipv4.conf.all.send_redirects=0
+    net.ipv4.conf.default.send_redirects=0
+
+    # protect against smurf attacks
+    net.ipv4.icmp_echo_ignore_all=1
+
+    # disable source routing to prevent man-in-the-middle
+    net.ipv4.conf.all.accept_source_route=0
+    net.ipv4.conf.default.accept_source_route=0
+    net.ipv6.conf.all.accept_source_route=0
+    net.ipv6.conf.default.accept_source_route=0
+
+    # disable ipv6 router advertisements
+    net.ipv6.conf.all.accept_ra=0
+    net.ipv6.conf.default.accept_ra=0
+
+    # disable tcp sack as it has been used commonly for exploits
+    net.ipv4.tcp_sack=0
+    net.ipv4.tcp_dsack=0
+    net.ipv4.tcp_fack=0
+
+    # disable tcp timestamps
+    net.ipv4.tcp_timestamps=0
+
+    # enable ipv6 privacy extensions on kernel level
+    net.ipv6.conf.all.use_tempaddr=2
+    net.ipv6.conf.default.use_tempaddr=2
+    EOF
 else
     echo "skipping kernel network hardening"
 fi
